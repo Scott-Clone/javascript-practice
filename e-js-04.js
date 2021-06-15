679c6658267f7383720f48c9f0bb4f3d2a68ace5
Carlone Scott <carlonescott@gmail.com>
2021-06-14 23:18:32 -0400

    Add deep copy solution
    This function takes two items and compares them if they are the same object by properties.
    
    If the two items are not objects they are compared using `===`

--- e-js-04.js
+++ e-js-04.js
@@ -58,15 +58,12 @@ function reverseArrayInPlace (arr) {
 
 console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
 
-
 // 3. A list
 
+// Function takes list and converts if to an array
 function arrayToList (arr) {
 
-    let list = {
-        value: arr[arr.length - 1],
-        rest: null
-    }
+    let list = null
     for (let i = arr.length - 1; i >= 0; i--) {
         list = {value: arr[i], rest: list}
     }
@@ -76,6 +73,7 @@ function arrayToList (arr) {
 let arr = [1, 2, 3]
 console.log(arrayToList(arr))
 
+// Function takes an array and converts if to a list
 function listToArray (list) {
     let arr = []
     for (let node = list; node; node = node.rest) {
@@ -87,6 +85,7 @@ function listToArray (list) {
 let list = { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
 console.log(listToArray(list))
 
+// function appends an element to a list
 function prepend(a, aList) {
     let newList = {
         value: a, rest: aList};
@@ -95,3 +94,41 @@ function prepend(a, aList) {
 
 console.log(prepend(10, prepend(20, null)));
 
+// function finds the nth value in a list
+function nth(aList, n) {
+     if (n == 0) {
+         return aList.value
+     } 
+     if (!aList) {
+        return undefined
+     } else {
+         return nth(aList.rest, n - 1)
+     } 
+}
+
+console.log(nth(list, 1))
+
+// Deep Comparison
+
+function deepEqual (a, b) {
+    if (a === b) return true;
+    if (typeof a == "object" && 
+        typeof b == "object" && 
+        a != null && b != null) {
+            let aKeys = Object.keys(a), bKeys = Object.keys(b)
+            if (aKeys.length == bKeys.length) {
+                for (let key of aKeys) {
+                    if (!bKeys.includes(key) || !deepEqual(a[key], b[key])) return false;
+                  }
+            } else return false;
+    } else return false;
+    return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(1, 1));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
